b5fbfbcccbfc5ac9e2706d2b3af6a3d7961a9358
dominikolszakolszewski <dominik.olszak.olszewski@gmail.com>
2026-02-04 21:16:00 +0100

    Update termometr.py

--- termometr.py
+++ termometr.py
@@ -108,7 +108,8 @@ def get_measurement(correction):
 
 # WYSYLANIE DANYCH
 def send_to_server(tid, temp):
-    url = f'http://192.168.1.106:8081/Thermo/Thermo?id={tid}&temperature={temp}'
+    #url = 'http://data-grabber.int.aluteam.pl:8081/Thermo/Thermo?id={TID}&temperature={TEM}'.format(TID = tid, TEM = round(temp, 1))
+    url = f'http://172.20.10.14:8081/Thermo/Thermo?id={TEM}&temperature={TEM}'.format(TID = tid, TEM = round(temp, 1)
     try:
         response = requests.get(url, timeout=5)
         return response.status_code == 200
@@ -153,3 +154,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
