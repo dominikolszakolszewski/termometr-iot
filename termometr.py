9cebbd9a6e1160803b6a8558b52b5f103c043f67
dominikolszakolszewski <dominik.olszak.olszewski@gmail.com>
2026-02-04 21:33:30 +0100

    Update termometr.py

--- termometr.py
+++ termometr.py
@@ -23,18 +23,16 @@ DATA = {
     },
 }
 
-# FUNKCJE SIECIOWE
 def wait_for_online(host):
     command = ['ping', '-c', '1', host]
     while True:
         try:
             if subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).returncode == 0:
                 return True
-        except Exception:
+        except:
             pass
         time.sleep(1)
 
-# KONTROLA WERSJI 
 def self_update():
     try:
         r = requests.get(github_url, timeout=10)
@@ -45,7 +43,7 @@ def self_update():
                 if 'current_version =' in line:
                     try:
                         remote_version = line.split('"')[1]
-                    except IndexError:
+                    except:
                         continue
                     break
             
@@ -53,12 +51,9 @@ def self_update():
                 with open(__file__, "w", encoding="utf-8") as f:
                     f.write(new_code)
                 os.execv(sys.executable, ['python3'] + sys.argv)
-            else:
-                continue
-    except Exception as e:
+    except:
         pass
 
-# IDENTYFIKACJA MAC
 def get_current_config():
     for iface in ("wlan0", "eth0", "usb0"):
         try:
@@ -69,86 +64,70 @@ def get_current_config():
                 for loc, devices in DATA.items():
                     if mac in devices:
                         config = devices[mac]
-                        config['location'] = loc
-                        config['mac'] = mac
+                        config['tid'] = config.get('tid')
                         return config
         except:
             continue
     return None
 
-# POMIAR TEMPERATURY
 def read_mcp9808():
     try:
         bus = smbus.SMBus(1)
         data = bus.read_i2c_block_data(0x18, 0x05, 2)
-        u, l = data[0] & 0x1F, data[1] / 16.0
-        return (u * 16 + l) - 256 if data[0] & 0x10 else u * 16 + l
-    except: return None
+        upper = data[0] & 0x1F
+        temp = (upper * 16.0) + (data[1] / 16.0)
+        if upper & 0x10:
+            temp -= 256.0
+        return temp
+    except:
+        return None
 
 def read_ds18b20():
     try:
         devices = glob.glob('/sys/bus/w1/devices/28*')
         if not devices: return None
-        folder = devices[0]
-        with open(folder + '/w1_slave', 'r') as f:
+        with open(devices[0] + '/w1_slave', 'r') as f:
             lines = f.readlines()
         pos = lines[1].find('t=')
         return float(lines[1][pos+2:]) / 1000.0 if pos != -1 else None
-    except: return None
+    except:
+        return None
 
 def get_measurement(correction):
     temp = read_mcp9808()
     if temp is None: temp = read_ds18b20()
-    if temp is not None:
-        return round(temp + correction, 1)
-    return None
+    return round(temp + correction, 1) if temp is not None else None
 
-# WYSYLANIE DANYCH
 def send_to_server(tid, temp):
-    #url = 'http://data-grabber.int.aluteam.pl:8081/Thermo/Thermo?id={TID}&temperature={TEM}'.format(TID = tid, TEM = round(temp, 1))
-    url = 'http://172.20.10.14:8081/Thermo/Thermo?id={TID}&temperature={TEM}'.format(TID=tid, TEM=round(temp, 1))
+    url = 'http://172.20.10.14:8081/Thermo/Thermo?id={TID}&temperature={TEM}'.format(TID = tid, TEM = round(temp, 1))
     try:
-        response = requests.get(url, timeout=5)
-        return response.status_code == 200
+        r = requests.get(url, timeout=5)
+        return r.status_code == 200
     except:
         return False
 
-# WLASCIWY PROGRAM
 def main():
     wait_for_online("8.8.8.8")
     self_update()
     
     config = get_current_config()
     if not config:
-        return 
+        sys.exit(1)
 
     tid = config.get('tid')
     correction = config.get('correction', 0)
 
     while True:
         try:
-            # Uzywamy czasu systemowego Raspberry Pi
-            now = time.strftime('%Y-%m-%d %H:%M:%S')
             temp_val = get_measurement(correction)
-            
             if temp_val is not None:
-                print(f"[{now}] Temp: {temp_val} C")
-                if send_to_server(tid, temp_val):
-                else:
-                    continue
-            else:
-                print(f"[{now}] Blad odczytu czujnika")
-                
+                send_to_server(tid, temp_val)
+            
             time.sleep(1)
         except KeyboardInterrupt:
-            break 
-        except Exception as e:
+            break
+        except:
             time.sleep(5)
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
